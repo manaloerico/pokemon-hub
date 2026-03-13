a6a60c2c1d5e94a4f3539a241b6527b7f8982ebf
Echo <manaloerico.games@gmail.com>
2026-03-13 20:08:25 +0800

    make my app a remote app

---
+++ src/bootstrap.js
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const mount = (el) => {
+	const root = ReactDOM.createRoot(el);
+	root.render(<App />);
+};
+
+// Auto-mount if running standalone (dev or GitHub Pages)
+if (!window.__IS_REMOTE_APP__) {
+	const devRoot = document.getElementById("root");
+	if (devRoot) mount(devRoot);
+}
+
+// Export mount for Angular host
+export { mount };
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+import federation from "@originjs/vite-plugin-federation";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
-	base: "/pokemon-hub/",
-	plugins: [react(), tailwindcss()],
+	base: "/pokemon-hub/", // needed for GitHub Pages standalone
+	plugins: [
+		react(),
+		tailwindcss(),
+		federation({
+			name: "pokemonHub",
+			filename: "remoteEntry.js",
+			exposes: {
+				"./App": "./src/bootstrap.js", // use bootstrap.js
+			},
+			shared: ["react", "react-dom"],
+		}),
+	],
+	build: {
+		target: "esnext",
+	},
 });
