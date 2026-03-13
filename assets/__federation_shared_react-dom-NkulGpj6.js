360d01b91bf36e3cf0df37c47d3c66ef5630f3e9
manaloerico <manaloerico@users.noreply.github.com>
2026-03-13 11:11:31 +0000

    Deploying to gh-pages from @ manaloerico/pokemon-hub@28680251137cbb116d0771e4392ac1912e4ae9ac 🚀

--- assets/__federation_shared_react-dom-NkulGpj6.js
+++
@@ -1,7 +0,0 @@
-import { g as getDefaultExportFromCjs } from './index-C0EA2977.js';
-import { r as requireReactDom } from './index-Y7FpfxD1.js';
-
-var reactDomExports = requireReactDom();
-const index = /*@__PURE__*/getDefaultExportFromCjs(reactDomExports);
-
-export { index as default };
