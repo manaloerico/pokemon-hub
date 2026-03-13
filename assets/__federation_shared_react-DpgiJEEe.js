41afcfcef70bd0c1a5518461fb026d94aaa5b9ba
manaloerico <manaloerico@users.noreply.github.com>
2026-03-13 09:25:19 +0000

    Deploying to gh-pages from @ manaloerico/pokemon-hub@3c361439bb56dacbd4f88bf96deeffc7c11c5325 🚀

---
+++ assets/__federation_shared_react-DpgiJEEe.js
@@ -0,0 +1,6 @@
+import { r as requireReact, g as getDefaultExportFromCjs } from './index-C0EA2977.js';
+
+var reactExports = requireReact();
+const index = /*@__PURE__*/getDefaultExportFromCjs(reactExports);
+
+export { index as default };
