715c1b25ff28923fa877b9a35816b216f055b0d3
Echo <manaloerico.games@gmail.com>
2026-03-14 14:07:14 +0800

    fix broken remoteapp css issue

--- src/remote.routes.tsx
+++ src/remote.routes.tsx
@@ -1,9 +1,10 @@
 import { HashRouter } from "react-router-dom";
 import ShadowWrapper from "./components/shadow-wrapper/shadow-wrapper.tsx";
+import styles from "./index.css?inline";
 import RoutesPage from "./pages/routes.tsx";
 function RemoteRoutes() {
 	return (
-		<ShadowWrapper>
+		<ShadowWrapper styles={styles}>
 			<HashRouter>
 				<RoutesPage />
 			</HashRouter>
