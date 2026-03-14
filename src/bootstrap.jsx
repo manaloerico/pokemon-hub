af3d193f0036a7755147fc55519e9fd4386a3ac2
Echo <manaloerico.games@gmail.com>
2026-03-14 09:49:01 +0800

    fix broken remoteapp css issue

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -1,15 +1,8 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
-
+import RemoteRoutes from "./remote.routes";
 const mount = (el) => {
 	const root = ReactDOM.createRoot(el);
-	root.render(<App />);
+	root.render(<RemoteRoutes />);
 };
-
-// Auto-mount for standalone
-if (!window.__IS_REMOTE_APP__) {
-	const devRoot = document.getElementById("root");
-	if (devRoot) mount(devRoot);
-}
 
 export { mount };
