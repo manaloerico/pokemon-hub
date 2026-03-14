3fb77c54525b96462fbe16f5ef7d48426c9da8ae
Echo <manaloerico.games@gmail.com>
2026-03-14 13:00:33 +0800

    fix broken remoteapp css issue

--- src/remote.routes.tsx
+++ src/remote.routes.tsx
@@ -1,12 +1,14 @@
 import { HashRouter } from "react-router-dom";
+import { ShadowRoot } from "react-shadow";
 import "./index.css";
 import RoutesPage from "./pages/routes.tsx";
-
 function RemoteRoutes() {
 	return (
-		<HashRouter>
-			<RoutesPage />
-		</HashRouter>
+		<ShadowRoot>
+			<HashRouter>
+				<RoutesPage />
+			</HashRouter>
+		</ShadowRoot>
 	);
 }
 
