f334932e2cf613721df6c1fd78764389242cd8a9
Echo <manaloerico.games@gmail.com>
2026-03-14 10:46:34 +0800

    fix broken remoteapp css issue

--- vite.config.js
+++ vite.config.js
@@ -21,15 +21,11 @@ export default defineConfig({
 	],
 	build: {
 		target: "esnext",
-		cssCodeSplit: false,
 		rollupOptions: {
 			output: {
-				assetFileNames: (assetInfo) => {
-					if (assetInfo.name?.endsWith(".css")) {
-						return "assets/remote.css";
-					}
-					return "assets/[name].[ext]";
-				},
+				chunkFileNames: "assets/[name]-[hash].js",
+				entryFileNames: "assets/[name]-[hash].js",
+				assetFileNames: "assets/[name]-[hash].[ext]",
 			},
 		},
 	},
